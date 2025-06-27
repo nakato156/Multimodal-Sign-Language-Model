df565d4bae0749d6c1f73bc3c02c5bdf0ee32874
R0SEWT <rody.vilchez00@gmail.com>
2025-06-26 22:56:48 -0500

    mc2 xd

--- src/mslm/dataloader/keypoint_dataset.py
+++ src/mslm/dataloader/keypoint_dataset.py
@@ -28,7 +28,8 @@ class KeypointDataset():
     def processData(self):
         with h5py.File(self.h5Path, 'r') as f:
             datasets  = list(f.keys())
-
+            datasets = sorted(datasets)
+        
             self.valid_index = []
             self.original_videos = []
 
@@ -87,18 +88,26 @@ class KeypointDataset():
         return filtered, stable_mask
 
     def keypoint_normalization(self, keypoint):
-        flat = keypoint.view(-1, 2)
-        global_mins, _ = flat.min(dim=0)
-        global_maxs, _ = flat.max(dim=0)
-
+        mask_keypoints = ~((keypoint[...,0] < 5) & (keypoint[...,1] < 5))
+        
+        valid_points = keypoint[mask_keypoints].view(-1, 2)
+        if valid_points.numel() > 0:
+            global_mins, _ = valid_points.min(dim=0)
+            global_maxs, _ = valid_points.max(dim=0)
+        else:
+            global_mins, _ = torch.zeros(2, device=keypoint.device)
+            global_maxs, _ = torch.ones(2, device=keypoint.device)
+                        
         global_ranges = global_maxs - global_mins
         global_ranges[global_ranges == 0] = 1.0
 
-        gm = global_mins.unsqueeze(0).unsqueeze(0)
-        gr = global_ranges.unsqueeze(0).unsqueeze(0)
+        normalized = (keypoint - global_mins) / global_ranges
 
-        return  (keypoint - gm) / gr
+        normalized[~mask_keypoints] = keypoint[~mask_keypoints] 
 
+        return normalized
+    
+    
     def apply_augmentation(self, keypoint, augmentation_type):
         """
         Aplica diferentes augmentaciones de acuerdo al tipo.
@@ -131,7 +140,7 @@ class KeypointDataset():
         keypoints_streched = streched.reshape(T_new, J, C)
         return keypoints_streched
     
-    def guassian_jitter(self, keypoint, sigma=5.0, clip=3.0):
+    def gaussian_jitter(self, keypoint, sigma=5.0, clip=3.0):
         keypoint_jitter = np.random.normal(loc=0.0, scale=sigma, size=keypoint.shape)
 
         if clip is not None:
@@ -142,7 +151,7 @@ class KeypointDataset():
     def rotation_2D(self, keypoint):
         # Aseguramos que la rotación no cambie la cantidad de keypoints
         angle = random.uniform(-15, 15)  # Rotación aleatoria en grados
-        angle_rad = np.deg2rad(angle)
+        angle_rad = torch.tensor(angle * np.pi / 180.0, dtype=torch.float32)  # Convertir a radianes
         rotation_matrix = torch.tensor([[torch.cos(angle_rad), -torch.sin(angle_rad)], 
                                        [torch.sin(angle_rad), torch.cos(angle_rad)]], dtype=torch.float32)
         keypoint_rotated = torch.matmul(keypoint.view(-1, 2), rotation_matrix)
@@ -165,6 +174,8 @@ class KeypointDataset():
 
     def __getitem__(self, idx):
         mapped_idx = self.valid_index[idx]
+
+        print(f"Mapped Index: {mapped_idx[0]}")
             
         with h5py.File(self.h5Path, 'r') as f:
             keypoint = f[mapped_idx[0]]["keypoints"][mapped_idx[1]][:]
@@ -174,17 +185,21 @@ class KeypointDataset():
                 label = f[mapped_idx[0]]["labels"][mapped_idx[1]][:][0].decode()
 
         if self.data_augmentation:
+            keypoint = torch.tensor(keypoint, dtype=torch.float32)
+            
             augmentation_type = self.data_augmentation_dict[mapped_idx[2]]
             keypoint = self.apply_augmentation(keypoint, augmentation_type)
-
-        #Keypoints a Tensor
-        keypoint = torch.tensor(keypoint, dtype=torch.float32)
         
-        # Keypoint Normalization
-        keypoint_normalized = self.keypoint_normalization(keypoint)
+        keypoint = torch.tensor(keypoint, dtype=torch.float32)
+        #Keypoints a Tensor
+        
         
         # Clean noise 
-        keypoint_normalized, _ = self.filter_unstable_keypoints_to_num(keypoint_normalized, self.n_keypoints)
+        keypoint, _ = self.filter_unstable_keypoints_to_num(keypoint, self.n_keypoints)
+
+        # Keypoint Normalization
+        keypoint_normalized = self.keypoint_normalization(keypoint)
+
 
         if self.return_label:
             return keypoint_normalized, torch.tensor(embedding), label
