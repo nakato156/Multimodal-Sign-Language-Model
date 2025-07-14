1176fcfe3b0911a276453109928e12d16de43424
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2025-07-14 13:36:00 -0500

    Minor changes

--- src/mslm/models/components/positional_encoding.py
+++ src/mslm/models/components/positional_encoding.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=5_000):
@@ -7,11 +8,12 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * -(torch.log(torch.tensor(10000.0)) / d_model))
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        log_scale = -math.log(10000.0) / d_model
+        div_term = torch.exp(torch.arange(0, d_model, 2) * log_scale)
 
-        pe[:, 0::2] = torch.sin(position.float() * div_term)
-        pe[:, 1::2] = torch.cos(position.float() * div_term)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
 
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
